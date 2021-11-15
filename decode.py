97b9a1856259a894c9785d6792e5ab94fe5a9dcd
John Comeau <jc@unternet.net>
2021-11-14 19:56:08 -0700

    varint not calculating correctly

--- decode.py
+++ decode.py
@@ -2,8 +2,11 @@
 '''
 Decode Google Mesh diagnostic-report
 '''
-import sys
+import sys, logging  # pylint: disable=multiple-imports
+
+logging.basicConfig(level=logging.DEBUG if __debug__ else logging.INFO)
 MARKER = b'\x12'  # ^R marks entries
+
 def decode(filename):
     '''
     Decode compressed document
@@ -12,6 +15,31 @@ def decode(filename):
         data = infile.read()
     offset = data.index(MARKER)
     print(data[:offset].decode())
+    while offset < len(data):
+        chunklength, offset = varint(data, offset + 1)
+        if data[offset + chunklength] != MARKER:
+            raise ValueError(
+                'Marker not found at 0x%x: %s' % (
+                    offset + chunklength,
+                    data[offset + chunklength:offset + chunklength + 64]
+                )
+            )
+
+def varint(data, offset):
+    '''
+    Decode variable integer and return new offset
+    '''
+    value = 0
+    while True:
+        byte = data[offset]
+        logging.debug('varint: byte=0x%x', byte)
+        value = (value << 7) + (byte & 0x7f)
+        offset += 1
+        if byte & 0x80 == 0:
+            break
+    logging.debug('value: 0x%x', value)
+    return value, offset
+
 if __name__ == '__main__':
     if len(sys.argv) == 1:
         sys.argv.append('diagnostic-report')
