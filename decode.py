f1c01b3073cd79bf70cb6daf9bcff7457254c927
John Comeau <jc@unternet.net>
2021-11-16 10:11:12 -0700

    starting new approach

--- decode.py
+++ decode.py
@@ -3,16 +3,16 @@
 Decode Google Mesh diagnostic-report
 '''
 import sys, logging  # pylint: disable=multiple-imports
+from collections import defaultdict
 
 logging.basicConfig(level=logging.DEBUG if __debug__ else logging.INFO)
-MARKER = b'\x12'  # ^R marks entries
-MARKERS = {
-    b'\x0a': 'filename',
-    MARKER: 'entry',
+MARKERS = defaultdict(lambda: 'unimplemented', {
+    b'\x0a': 'string',
+    b'\x12': 'entry',
     b'\x1a': 'sysctl',
-    b'"': 'unknown0',  # b'\x22'
-    b'*': 'unknown1',  # b'\x2a'
-}
+    b'\x22': 'unknown0',  # b'"'
+    b'\x2a': 'unknown1',  # b'*'
+})
 
 def decode(filename):
     '''
@@ -20,9 +20,22 @@ def decode(filename):
     '''
     with open(filename, 'rb') as infile:
         data = infile.read()
-    offset = data.index(MARKER)
-    print(data[:offset].decode())
+    offset = 0
     while offset < len(data):
+        marker = data[offset:offset + 1]
+        logging.debug('marker: %r (%s)', marker, MARKERS[marker])
+        # pylint: disable=eval-used
+        offset = eval(MARKERS[marker])(data, offset + 1)
+
+def oldstuff(filename):
+    '''
+    initial code
+    '''
+    with open(filename, 'rb') as infile:
+        data = infile.read()
+    offset = data.index('\x12')
+    print(data[:offset].decode())
+    while False:
         chunklength, offset = varint(data, offset + 1)
         if data[offset + chunklength:offset + chunklength + 1] not in MARKERS:
             raise ValueError(
@@ -32,7 +45,7 @@ def decode(filename):
                 )
             )
         end_chunk = offset + chunklength
-        if data[offset:offset + 1] != b'\x0a':
+        if data[offset:offset + 1] not in MARKERS:
             raise ValueError(
                 'Filename not found at 0x%x: %s' % (
                     offset,
@@ -44,7 +57,7 @@ def decode(filename):
         filename = data[offset:offset + namelength].decode()
         logging.debug('processing file: %s', filename)
         offset += namelength
-        if data[offset:offset + 1] != MARKER:
+        if data[offset:offset + 1] != '\x12':
             raise ValueError(
                 'Marker not found at 0x%x: %s' % (
                     offset + chunklength,
@@ -70,6 +83,21 @@ def decode(filename):
                 )
             )
         offset = end_chunk
+
+def string(data, offset):
+    '''
+    retrieve variable length string from data
+    '''
+    bytestring, offset = varbytes(data, offset)
+    print(bytestring.decode())
+    return offset
+
+def varbytes(data, offset):
+    '''
+    Get variable length string and return new offset
+    '''
+    length, offset = varint(data, offset)
+    return data[offset:offset + length], offset + length
 
 def varint(data, offset):
     '''
